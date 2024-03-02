b2dac4758aac68d7cdf9cae798b6241a13474340
Abdullah Khan <iiabdullahii.khan@gmail.com>
2024-03-02 12:55:03 +0500

    Connected to the Database

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
 const app = require("./app");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Database connection successfull!!");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("Listening to the port 3000!");
